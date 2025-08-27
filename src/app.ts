ad3031157c6ff3ebbb2d64ba2cc078e8840a2181
maiquegouveiabr <maiquegouveiabr@gmail.com>
2025-08-27 20:33:42 -0300

    fix: added cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
 import express from 'express';
 import cookiesRouter from './routes/cookiesRoutes.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import cors from 'cors';
+
+const allowedOrigins = [
+  'https://referralcuritiba.vercel.app',
+  'https://referralcuritibasouth.vercel.app',
+  'http://localhost:3000',
+];
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  }),
+);
 
 // Routes
 app.use('/api/cookies', cookiesRouter);
